a07f087f8d1486ab6d8f757e489cad3d3fc6b5da
Radek Podgorny <radek@podgorny.cz>
2014-03-13 21:29:37 +0100

    custom tweaks for my setup

--- send.cpp
+++ send.cpp
@@ -14,15 +14,23 @@ int main(int argc, char *argv[]) {
      see https://projects.drogon.net/raspberry-pi/wiringpi/pins/
      for pin mapping of the raspberry pi GPIO connector
      */
-    int PIN = 0;
+    int PIN = 7;
     char* systemCode = argv[1];
     int unitCode = atoi(argv[2]);
     int command  = atoi(argv[3]);
+
+	// it seems to be inverted for my case
+	if (command == 0) {
+		command = 1;
+	} else {
+		command = 0;
+	}
     
     if (wiringPiSetup () == -1) return 1;
 	printf("sending systemCode[%s] unitCode[%i] command[%i]\n", systemCode, unitCode, command);
 	RCSwitch mySwitch = RCSwitch();
 	mySwitch.enableTransmit(PIN);
+	mySwitch.setPulseLength(100);  // found this constant using oscilloscope
     
     switch(command) {
         case 1:
